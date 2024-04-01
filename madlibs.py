0efd777021108e9cf2977e32654a992c95fdfe3e
Anushka Singhal <92168276+anushka-singhal@users.noreply.github.com>
2024-04-01 12:36:13 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,7 @@
+adj = input("Adjective: ")
+verb1= input("verb: ")
+verb2= input("verb: ")
+famous_person = input("famous person: ")
+madlib = f"Computer programming is so {adj}! it makes me to exicted all the time because\
+i love to {verb1}. stay hydrated and {verb2} like you are {famous_person}! "
+print(madlib)
